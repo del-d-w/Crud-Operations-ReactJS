70bc965de54f945bcea1d260166fcd256d9936db
del-d-w <106804698+del-d-w@users.noreply.github.com>
2024-01-05 18:38:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "./store";
-import { Provider } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
@@ -14,9 +12,7 @@ store.subscribe(() => console.log(store.getState()))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
     <App />
-    </Provider>
   </React.StrictMode>,
 );
 
