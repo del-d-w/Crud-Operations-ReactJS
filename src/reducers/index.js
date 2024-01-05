41f0815a4f8799fc28d88b9fe563f748b06b76d5
del-d-w <106804698+del-d-w@users.noreply.github.com>
2024-01-05 18:37:05 +0530

    Delete src/reducers directory

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// reducers/index.js
-import { combineReducers } from "redux";
-import changeTheNumber from "./upDown";
-
-const rootReducer = combineReducers({
-    changeTheNumber
-});
-
-export default rootReducer;
